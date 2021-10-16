f9f62969da76429a77a39bec657e57d14e826c6d
Omer Citik <omer.citik@hva.nl>
2021-10-16 22:05:35 +0300

    Little refactor

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,17 +1,69 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import './App.css';
+import KeyRow from './components/KeyRow'
 
 function App() {
+
+  const keyData = [
+    {
+      "AC": { isNumber: false, isOperation: false },
+      "+/-": { isNumber: false, isOperation: false },
+      "%": { isNumber: false, isOperation: true },
+      "/": { isNumber: false, isOperation: true },
+    },
+    {
+      "7": { isNumber: true, isOperation: true },
+      "8": { isNumber: true, isOperation: true },
+      "9": { isNumber: true, isOperation: true },
+      "*": { isNumber: false, isOperation: true },
+    },
+    {
+      "4": { isNumber: true, isOperation: true },
+      "5": { isNumber: true, isOperation: true },
+      "6": { isNumber: true, isOperation: true },
+      "-": { isNumber: false, isOperation: true },
+    },
+    {
+      "1": { isNumber: true, isOperation: true },
+      "2": { isNumber: true, isOperation: true },
+      "3": { isNumber: true, isOperation: true },
+      "+": { isNumber: false, isOperation: true },
+    },
+    {
+      "0": { isNumber: true, isOperation: true },
+      ".": { isNumber: true, isOperation: true },
+      "=": { isNumber: false, isOperation: true },
+    },
+  ]
+
+  const allowedKeys = {
+    "0": { isNumber: true, isOperation: true },
+    "1": { isNumber: true, isOperation: true },
+    "2": { isNumber: true, isOperation: true },
+    "3": { isNumber: true, isOperation: true },
+    "4": { isNumber: true, isOperation: true },
+    "5": { isNumber: true, isOperation: true },
+    "6": { isNumber: true, isOperation: true },
+    "7": { isNumber: true, isOperation: true },
+    "8": { isNumber: true, isOperation: true },
+    "9": { isNumber: true, isOperation: true },
+    ".": { isNumber: true, isOperation: true },
+    "/": { isNumber: false, isOperation: true },
+    "+": { isNumber: false, isOperation: true },
+    "-": { isNumber: false, isOperation: true },
+    "*": { isNumber: false, isOperation: true },
+    "%": { isNumber: false, isOperation: true },
+    "=": { isNumber: false, isOperation: true },
+  }
 
   const [operations, setOperations] = useState([])
   const [isLastOperation, setIsLastOperation] = useState(false)
   const [displayValue, setDisplayValue] = useState("")
 
   const [negative, setNegative] = useState(false)
-  const [canPlaceDot,setCanPlaceDot] = useState(true)
+  const [canPlaceDot, setCanPlaceDot] = useState(true)
 
   const Parser = require('expr-eval').Parser;
-
 
   const setClickedButton = (e, isNumber, isOperation) => {
     e.preventDefault()
@@ -22,27 +74,29 @@ function App() {
     if (isNumber) {
       setIsLastOperation(false)
 
-      if (value==='.'){
-        if(canPlaceDot){
-          const lastCharParsed = parseInt (displayValue.substr(displayValue.length - 1))
-          if(isNaN(lastCharParsed)){
-            setDisplayValue(displayValue + "0"+value)
+      if (value === '.') {
+        if (canPlaceDot) {
+          const lastCharParsed = parseInt(displayValue.substr(displayValue.length - 1))
+          if (isNaN(lastCharParsed)) {
+            setDisplayValue(displayValue + "0" + value)
             setIsLastOperation(false)
             setCanPlaceDot(false)
-          }else{
+          } else {
             setDisplayValue(displayValue + value)
             setIsLastOperation(false)
             setCanPlaceDot(false)
           }
 
-        } 
-      }else{
+        }
+      } else {
+
         setDisplayValue(displayValue + value)
+        console.log("Hey you should add value to displayValue", value, displayValue)
         setIsLastOperation(false)
       }
-      
 
-    } else if (isOperation && displayValue.length>0) {
+
+    } else if (isOperation && displayValue.length > 0) {
       setCanPlaceDot(true)
       if (isLastOperation) {
         // Remove last operator char from displayvalue en replace with new operator
@@ -101,15 +155,39 @@ function App() {
         let expression = parser.parse(displayValue);
         let result = expression.evaluate({ x: 3 });
 
-        setDisplayValue(isNaN(result)?"Error":result.toString())
+        setDisplayValue(isNaN(result) ? "Error" : result.toString())
         setOperations([])
-        setCanPlaceDot(result.toString().includes(".")?false:true)
+        setCanPlaceDot(result.toString().includes(".") ? false : true)
         setNegative((result < 0))
       } catch (e) {
 
       }
     }
   }
+
+  const handleKeyPress = (event) => {
+    console.log(event.key)
+    if (event.key === '=' || event.key === 'Enter') {
+      console.log("Yo I should do calculation")
+      calculate(event)
+    } else {
+      const pressedKey = allowedKeys[event.key]
+
+      event.target.value = event.key
+      if (pressedKey) {
+        setClickedButton(event, pressedKey.isNumber, pressedKey.isOperation)
+      }
+    }
+
+
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    return function cleanupListener() {
+      document.removeEventListener('keydown', handleKeyPress)
+    }
+  }, [displayValue, isLastOperation, negative, canPlaceDot])
 
   return (
     <div className="calculator">
@@ -118,35 +196,12 @@ function App() {
           {/* display result */}
           <h4>{displayValue}</h4>
         </div>
-        <div className="calculator-row">
-          <button onClick={(e) => setClickedButton(e, false, false)} value="AC">AC</button>
-          <button onClick={(e) => setClickedButton(e, false, false)} value="+/-">+/-</button>
-          <button onClick={(e) => setClickedButton(e, false, true)} value="%">%</button>
-          <button onClick={(e) => setClickedButton(e, false, true)} value="/" style={{ fontSize: "1rem" }}>&divide;</button>
-        </div>
-        <div className="calculator-row">
-          <button onClick={(e) => setClickedButton(e, true, false)} value="7">7</button>
-          <button onClick={(e) => setClickedButton(e, true, false)} value="8">8</button>
-          <button onClick={(e) => setClickedButton(e, true, false)} value="9">9</button>
-          <button onClick={(e) => setClickedButton(e, false, true)} value="*">x</button>
-        </div>
-        <div className="calculator-row">
-          <button onClick={(e) => setClickedButton(e, true, false)} value="4">4</button>
-          <button onClick={(e) => setClickedButton(e, true, false)} value="5">5</button>
-          <button onClick={(e) => setClickedButton(e, true, false)} value="6">6</button>
-          <button onClick={(e) => setClickedButton(e, false, true)} value="-">-</button>
-        </div>
-        <div className="calculator-row">
-          <button onClick={(e) => setClickedButton(e, true, false)} value="1">1</button>
-          <button onClick={(e) => setClickedButton(e, true, false)} value="2">2</button>
-          <button onClick={(e) => setClickedButton(e, true, false)} value="3">3</button>
-          <button onClick={(e) => setClickedButton(e, false, true)} value="+">+</button>
-        </div>
-        <div className="calculator-row">
-          <button onClick={(e) => setClickedButton(e, true, false)} value="0">0</button>
-          <button onClick={(e) => setClickedButton(e, true, false)} value=".">.</button>
-          <button onClick={(e) => calculate(e)} value="=">=</button>
-        </div>
+
+        {keyData.map((keyRow,index) => {
+          return <KeyRow key={index} keyRow={keyRow} setClickedButton={setClickedButton} calculate={calculate}/>
+        })}
+
+
 
       </div>
     </div>
